344adfa6a522754ba778c62a8bca9227836a9aad
Nguyễn Đăng Cường <dangcuong201004@gmail.com>
2025-12-17 01:52:41 +0700

    pre Mvc code for ndcuong

---
+++ src/main/java/vn/ndcuong/laptopshop/service/MyService.java
@@ -0,0 +1,8 @@
+package vn.ndcuong.laptopshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+}
--- src/test/java/vn/ndcuong/laptopshop/LaptopshopApplicationTests.java
+++ src/test/java/vn/ndcuong/laptopshop/LaptopshopApplicationTests.java
@@ -1,4 +1,4 @@
-package vn.hoidanit.laptopshop;
+package vn.ndcuong.laptopshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
